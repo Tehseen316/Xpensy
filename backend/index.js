3c6052c75078d6706584d28931a7208fdc09d18e
Tehseen316 <tehseena316@gmail.com>
2025-07-09 01:29:34 +0100

    Index Added

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,34 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRouter = require("./routes/userRouter");
+const errorHandler = require("./middlewares/errorHandlerMiddleware");
+const categoryRouter = require("./routes/categoryRouter");
+const transactionRouter = require("./routes/transactionRouter");
+const app = express();
 
-dotenv.config();
+//!Connect to mongodb
+mongoose
+  .connect("mongodb://localhost:27017/mern-expenses")
+  .then(() => console.log("DB Connected"))
+  .catch((e) => console.log(e));
 
+//! Cors config
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
+app.use(cors(corsOptions));
+//!Middlewares
+app.use(express.json()); //?Pass incoming json data
+//!Routes
+app.use("/", userRouter);
+app.use("/", categoryRouter);
+app.use("/", transactionRouter);
+//! Error
+app.use(errorHandler);
 
-const app = express()
-
-app.use(cors())
-
-app.use(express.json())
-
-
-const PORT = process.env.PORT
-
-app.listen('/', ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+//!Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () =>
+  console.log(`Server is running on this port... ${PORT} `)
+);
